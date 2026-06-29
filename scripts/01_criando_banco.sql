978a0b96ea24e5297a39fddefa4590c7f4df0ac8
Igor Cruz <igorcruzlsls@gmail.com>
2026-06-29 01:21:34 -0300

    Subindo remodelagem do banco - arrumar inserts da bb faker

--- scripts/01_criando_banco.sql
+++ scripts/01_criando_banco.sql
@@ -1,7 +1,6 @@
 USE master;
 GO
 
-
 CREATE DATABASE monitoramento_db;
 GO
 
@@ -15,6 +14,8 @@ CREATE TABLE clientes
     nome VARCHAR(100) NOT NULL,
     cidade VARCHAR(50) NULL,
     estado CHAR(2) NOT NULL,
+    bairro VARCHAR(80) NULL,
+    cep CHAR(8) NULL,
 
     CONSTRAINT PK_Clientes
         PRIMARY KEY (id_cliente)
@@ -25,11 +26,9 @@ CREATE TABLE veiculos
 (
     id_veiculo INT IDENTITY(1,1) NOT NULL,
 
-    modelo VARCHAR(50) NOT NULL, 
-
+    modelo VARCHAR(50) NOT NULL,
     placa CHAR(7) NOT NULL,
     capacidade_kg INT NULL,
-
     categoria CHAR(1) NOT NULL,
 
     CONSTRAINT PK_Veiculos
@@ -56,15 +55,69 @@ CREATE TABLE centros_distribuicao
 );
 GO
 
+CREATE TABLE faixas_peso
+(
+    id_faixa_peso TINYINT NOT NULL,
+
+    descricao VARCHAR(50) NOT NULL,
+    peso_min_kg DECIMAL(10,3) NULL,
+    peso_max_kg DECIMAL(10,3) NULL,
+
+    CONSTRAINT PK_FaixasPeso
+        PRIMARY KEY (id_faixa_peso),
+
+    CONSTRAINT CK_FaixasPeso_Limites
+        CHECK (
+            peso_min_kg IS NULL
+            OR peso_max_kg IS NULL
+            OR peso_max_kg >= peso_min_kg
+        )
+);
+GO
+
+CREATE TABLE produtos
+(
+    id_produto INT IDENTITY(1,1) NOT NULL,
+
+    nome VARCHAR(100) NOT NULL,
+    categoria VARCHAR(50) NOT NULL,
+    id_faixa_peso TINYINT NOT NULL,
+
+    CONSTRAINT PK_Produtos
+        PRIMARY KEY (id_produto),
+
+    CONSTRAINT FK_Produtos_FaixaPeso
+        FOREIGN KEY (id_faixa_peso)
+        REFERENCES faixas_peso(id_faixa_peso)
+);
+GO
+
+CREATE TABLE status_entrega
+(
+    id_status_entrega TINYINT NOT NULL,
+
+    descricao VARCHAR(50) NOT NULL,
+
+    CONSTRAINT PK_StatusEntrega_Dominio
+        PRIMARY KEY (id_status_entrega),
+
+    CONSTRAINT UQ_StatusEntrega_Descricao
+        UNIQUE (descricao)
+);
+GO
+
 CREATE TABLE fato_envios
 (
     id_envio INT IDENTITY(1,1) NOT NULL,
 
     id_cliente INT NOT NULL,
     id_veiculo INT NOT NULL,
+    id_cd_origem INT NULL,
+    id_cd_destino INT NULL,
 
     data_postagem DATETIME NOT NULL,
     data_previsao_entrega DATETIME NOT NULL,
+    data_entrega_real DATETIME NULL,
 
     valor_frete DECIMAL(10,2) NOT NULL,
     peso_carga DECIMAL(10,3) NOT NULL,
@@ -80,6 +133,14 @@ CREATE TABLE fato_envios
         FOREIGN KEY (id_veiculo)
         REFERENCES veiculos(id_veiculo),
 
+    CONSTRAINT FK_Envios_CDOrigem
+        FOREIGN KEY (id_cd_origem)
+        REFERENCES centros_distribuicao(id_cd),
+
+    CONSTRAINT FK_Envios_CDDestino
+        FOREIGN KEY (id_cd_destino)
+        REFERENCES centros_distribuicao(id_cd),
+
     CONSTRAINT CK_Envios_ValorFrete
         CHECK (valor_frete > 0),
 
@@ -87,7 +148,43 @@ CREATE TABLE fato_envios
         CHECK (peso_carga > 0),
 
     CONSTRAINT CK_Envios_Datas
-        CHECK (data_previsao_entrega >= data_postagem)
+        CHECK (data_previsao_entrega >= data_postagem),
+
+    CONSTRAINT CK_Envios_DataEntregaReal
+        CHECK (data_entrega_real IS NULL OR data_entrega_real >= data_postagem)
+);
+GO
+
+CREATE TABLE itens_envio
+(
+    id_item_envio INT IDENTITY(1,1) NOT NULL,
+
+    id_envio INT NOT NULL,
+    id_produto INT NOT NULL,
+
+    quantidade INT NOT NULL,
+    peso_unitario DECIMAL(10,3) NULL,
+    valor_unitario DECIMAL(10,2) NULL,
+
+    CONSTRAINT PK_ItensEnvio
+        PRIMARY KEY (id_item_envio),
+
+    CONSTRAINT FK_ItensEnvio_Envio
+        FOREIGN KEY (id_envio)
+        REFERENCES fato_envios(id_envio),
+
+    CONSTRAINT FK_ItensEnvio_Produto
+        FOREIGN KEY (id_produto)
+        REFERENCES produtos(id_produto),
+
+    CONSTRAINT CK_ItensEnvio_Quantidade
+        CHECK (quantidade > 0),
+
+    CONSTRAINT CK_ItensEnvio_PesoUnitario
+        CHECK (peso_unitario IS NULL OR peso_unitario > 0),
+
+    CONSTRAINT CK_ItensEnvio_ValorUnitario
+        CHECK (valor_unitario IS NULL OR valor_unitario >= 0)
 );
 GO
 
@@ -97,8 +194,8 @@ CREATE TABLE fato_status_entrega
 
     id_envio INT NOT NULL,
     id_cd INT NOT NULL,
+    id_status_entrega TINYINT NOT NULL,
 
-    status VARCHAR(50) NOT NULL,
     ultima_atualizacao DATETIME NOT NULL,
 
     CONSTRAINT PK_StatusEntrega
@@ -110,27 +207,21 @@ CREATE TABLE fato_status_entrega
 
     CONSTRAINT FK_Status_CD
         FOREIGN KEY (id_cd)
-        REFERENCES centros_distribuicao(id_cd)
+        REFERENCES centros_distribuicao(id_cd),
+
+    CONSTRAINT FK_Status_StatusEntrega
+        FOREIGN KEY (id_status_entrega)
+        REFERENCES status_entrega(id_status_entrega)
 );
 GO
 
-CREATE INDEX IX_Envios_Cliente
-ON fato_envios(id_cliente);
-
-CREATE INDEX IX_Envios_Veiculo
-ON fato_envios(id_veiculo);
-
-CREATE INDEX IX_Status_Envio
-ON fato_status_entrega(id_envio);
-
-CREATE INDEX IX_Status_CD
-ON fato_status_entrega(id_cd);
-GO
-
-SELECT * FROM clientes;
-SELECT * FROM veiculos;
-SELECT * FROM centros_distribuicao;
-SELECT * FROM fato_envios;
-SELECT * FROM fato_status_entrega;
-GO
-
+INSERT INTO faixas_peso
+(
+    id_faixa_peso,
+    descricao,
+    peso_min_kg,
+    peso_max_kg
+)
+VALUES
+(1, 'Leve', 0.001, 5.000),
+(2, 'Medio', 5.001, 30)
--- scripts/03_joins.sql
+++ scripts/03_joins.sql
@@ -1,48 +1,95 @@
-USE monitoramento_db
+use monitoramento_db
 
--- VIEW COM INNER JOIN PARA RETORNAR OS DADOS DO ENVIO
+-- VIEW PARA RETORNAR OS DADOS GERAIS DO ENVIO
 
-
-CREATE VIEW vw_envio AS
-
+CREATE OR ALTER VIEW vw_envio AS
 SELECT 
-e.id_envio,
-c.nome,
-v.modelo,
-e.data_postagem,
-e.data_previsao_entrega,
-e.valor_frete,
-e.peso_carga
+    e.id_envio,
 
-FROM 
-	fato_envios e
+    c.id_cliente,
+    c.nome AS cliente,
+    c.cidade AS cidade_cliente,
+    c.estado AS estado_cliente,
 
-INNER JOIN 
-	clientes c ON c.id_cliente = e.id_cliente
-INNER JOIN
-	veiculos v ON v.id_veiculo = e.id_veiculo
+    v.id_veiculo,
+    v.modelo AS veiculo,
+    v.placa,
+    v.categoria AS categoria_veiculo,
+
+    cd_origem.nome_hub AS cd_origem,
+    cd_origem.cidade AS cidade_origem,
+    cd_origem.estado AS estado_origem,
+
+    cd_destino.nome_hub AS cd_destino,
+    cd_destino.cidade AS cidade_destino,
+    cd_destino.estado AS estado_destino,
+
+    e.data_postagem,
+    e.data_previsao_entrega,
+    e.data_entrega_real,
+
+    e.valor_frete,
+    e.peso_carga
+
+FROM fato_envios e
+INNER JOIN clientes c
+    ON c.id_cliente = e.id_cliente
+INNER JOIN veiculos v
+    ON v.id_veiculo = e.id_veiculo
+LEFT JOIN centros_distribuicao cd_origem
+    ON cd_origem.id_cd = e.id_cd_origem
+LEFT JOIN centros_distribuicao cd_destino
+    ON cd_destino.id_cd = e.id_cd_destino;
 
 
-SELECT * FROM vw_envio
-
-
--- VIEW COM INNER JOIN PARA RETORNAR OS DADOS DE ENTREGA E STATUS
-
+-- VIEW PARA RETORNAR OS DADOS DE ENTREGA E STATUS
 
 CREATE VIEW vw_entrega AS
-
 SELECT 
-e.id_status,
-e.id_envio,
-c.nome_hub,
-e.status,
-e.ultima_atualizacao
+    fse.id_status,
+    fse.id_envio,
 
-FROM fato_status_entrega e
-INNER JOIN centros_distribuicao c ON c.id_cd = e.id_cd
+    cd.id_cd,
+    cd.nome_hub,
+    cd.cidade,
+    cd.estado,
 
-SELECT * FROM vw_entrega
+    se.id_status_entrega,
+    se.descricao AS status_entrega,
 
+    fse.ultima_atualizacao
 
+FROM fato_status_entrega fse
+INNER JOIN centros_distribuicao cd
+    ON cd.id_cd = fse.id_cd
+INNER JOIN status_entrega se
+    ON se.id_status_entrega = fse.id_status_entrega;
 
-SELECT * FROM fato_status_entrega
+SELECT * FROM vw_entrega;
+
+-- VIEW PARA RETORNAR OS PRODUTOS DE CADA ENVIO
+
+CREATE VIEW vw_itens_envio AS
+SELECT
+    ie.id_item_envio,
+    ie.id_envio,
+
+    p.id_produto,
+    p.nome AS produto,
+    p.categoria AS categoria_produto,
+
+    fp.id_faixa_peso,
+    fp.descricao AS faixa_peso,
+
+    ie.quantidade,
+    ie.peso_unitario,
+    ie.valor_unitario,
+
+    (ie.quantidade * ISNULL(ie.peso_unitario, 0)) AS peso_total_item,
+    (ie.quantidade * ISNULL(ie.valor_unitario, 0)) AS valor_total_item
+
+FROM itens_envio ie
+INNER JOIN produtos p
+    ON p.id_produto = ie.id_produto
+INNER JOIN faixas_peso fp
+    ON fp.id_faixa_peso = p.id_faixa_peso;
