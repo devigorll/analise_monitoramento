ec7cd83c01540c4599697e016a7f7cf8711a6246
Igor Cruz <igorcruzlsls@gmail.com>
2026-07-14 00:49:59 -0300

    Subindo alterações no dashboard em PB

--- scripts/03_joins.sql
+++ scripts/03_joins.sql
@@ -110,3 +110,37 @@ INNER JOIN faixas_peso fp
         AND
         (fp.peso_max_kg IS NULL OR e.peso_carga <= fp.peso_max_kg)
     );
+
+
+-- VIEW PARA RETORNAR MAIS INFORMAÇÕES DO PROODUTO
+
+
+CREATE OR ALTER VIEW vw_produtos AS
+SELECT 
+	a.id_envio,
+	a.id_produto,
+	a.quantidade,
+	a.valor_unitario,
+
+	b.nome,
+	b.categoria
+
+FROM itens_envio a 
+INNER JOIN produtos b
+ON a.id_produto = b.id_produto
+
+SELECT * FROM vw_produtos
+
+
+-- VIEW PARA CRIAR GRÁFICO CATEGORIA X RECEITA
+
+CREATE OR ALTER VIEW vw_categoria_receita AS
+SELECT 
+    fp.id_faixa_peso,
+    fe.peso_carga AS peso,
+    fp.descricao,
+    fe.valor_frete
+FROM fato_envios AS fe
+INNER JOIN faixas_peso AS fp 
+    ON fe.peso_carga >= COALESCE(fp.peso_min_kg, 0)
+    AND fe.peso_carga <= COALESCE(fp.peso_max_kg, 999999.999);
